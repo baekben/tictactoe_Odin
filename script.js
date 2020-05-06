04ac285ba9962a68c061d69268e459fa4412c5e0
Ben <ben10baek@hotmail.com>
2020-05-05 17:07:19 -0700

    Update to add Xs and Os to board and array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
   const board = ['', '', '', '', '', '', '', '', '']; //3x3
-  const getBoard = () => board;
+  //const getBoard = () => board;
   //const resetBoards = (currentboard) =>(//additems for current board here)
-  return getBoard;
+  return board;
 })();
 const player = (name, icon) => {
   const getName = () => name; //player name
@@ -13,16 +13,39 @@ const player = (name, icon) => {
 
 const displayController = (() => {
   //displays gameboard on screen
-  const playerMark = () => {};
-  document.addEventListener('click', function (event) {
+  var player = 1;
+  const screenBoard = document.getElementById('board');
+  const turn = document.getElementById('turn');
+  const playerMark = (event) => {
     var value = event.target;
-    this.console.log(value.dataset.square);
+    const box = value.getAttribute('data-square');
     if (value.innerHTML == '') {
-      value.innerHTML = 'X'; //player 1 and player 2 will be O
+      if (player == 1) {
+        value.innerHTML = 'X';
+        player = 2;
+      } else {
+        value.innerHTML = 'O';
+        player = 1;
+      }
+      //player 1 and player 2 will be O
     } else {
-      this.alert('Error');
+      alert('Error');
     }
+    gameBoard[box] = value.innerHTML;
+    turn.innerHTML = `Player ${player}'s turn`;
+    console.log(`Player ${player}'s turn`);
+  };
+  const gameUpdate = (e) => {
+    //squares = e.target.querySelectorAll('.square');
+    box = e.target.getAttribute('data-square');
+    console.log(gameBoard);
+  };
+  screenBoard.addEventListener('click', (e) => {
+    playerMark(e);
+    gameUpdate(e);
   });
+
+  return { screenBoard };
 })();
 
 const gameController = (() => {
